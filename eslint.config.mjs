e50dd83f30efaa7efdd2810f3a3e451929fc6d3f
Xabier Echezurieta <xabier.echezurieta@gmail.com>
2025-02-07 21:58:42 +0100

    Add Prettier and ESLint configuration with Tailwind CSS support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,20 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import neostandard from 'neostandard'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+	...compat.extends('next/core-web-vitals', 'next/typescript'),
+	...neostandard(),
+	eslintConfigPrettier
+]
 
-export default eslintConfig;
+export default eslintConfig
