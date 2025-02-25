ad34cb04fb733fdaf2fcdd48f68068d14588eaba
Xabier Echezurieta <xabier.echezurieta@gmail.com>
2025-02-25 19:51:34 +0100

    Update ESLint configuration to enable TypeScript support in Neostandard

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
 	...compat.extends('next/core-web-vitals', 'next/typescript'),
-	...neostandard(),
+	...neostandard({ ts: true }),
 	eslintConfigPrettier
 ]
 
