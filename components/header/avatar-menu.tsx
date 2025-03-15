1d20493d1d6476ac06a3e8e3ee838acfa37d5854
Xabier Echezurieta <xabier.echezurieta@gmail.com>
2025-03-15 12:35:10 +0100

    refactor: simplify avatar menu by removing unused AvatarImage and updating fallback text

--- components/header/avatar-menu.tsx
+++ components/header/avatar-menu.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { LogOut } from 'lucide-react'
 
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
 import {
 	DropdownMenu,
 	DropdownMenuContent,
@@ -9,16 +9,18 @@ import {
 	DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
 export default function AvatarMenu() {
+	const handleLogout = () => {
+		console.log('Logout')
+	}
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
 				<Avatar className='cursor-pointer'>
-					<AvatarImage src='https://ui-avatars.com/api/?background=random' />
-					<AvatarFallback>UN</AvatarFallback>
+					<AvatarFallback>AV</AvatarFallback>
 				</Avatar>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent align='end' className='w-48'>
-				<DropdownMenuItem onClick={() => console.log('Logout clicked')}>
+				<DropdownMenuItem onClick={() => handleLogout()}>
 					<LogOut className='mr-2 size-4' />
 					<span>Log out</span>
 				</DropdownMenuItem>
